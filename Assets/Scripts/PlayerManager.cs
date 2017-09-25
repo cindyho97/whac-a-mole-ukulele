8394eedb00c66a7c57105ea027b3890720b98c9c
cindyho97 <cindy.ho8197@gmail.com>
2017-09-25 17:06:44 +0200

    Add managers and listeners

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,4 +5,37 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour  {
 
     private byte lives = 5;
+    private ushort score;
+
+
+    private void Awake()
+    {
+        Messenger.AddListener(GameEvent.LOSE_LIFE, LoseLife);
+        Messenger.AddListener(GameEvent.UPDATE_SCORE, UpdateScore);
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.LOSE_LIFE, LoseLife);
+        Messenger.RemoveListener(GameEvent.UPDATE_SCORE, UpdateScore);
+    }
+    private void LoseLife()
+    {
+        lives--;
+
+        if(lives < 0)
+        {
+            lives = 0;
+        }
+
+        if(lives == 0)
+        {
+            Messenger.Broadcast(GameEvent.LEVEL_FAILED);
+        }
+    }
+
+    private void UpdateScore()
+    {
+        score++;
+    }
 }
