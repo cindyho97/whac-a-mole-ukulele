1880bad2605eefe00da829820cd2e9ba5d49cb4e
cindyho97 <cindy.ho8197@gmail.com>
2017-09-25 14:54:56 +0200

    Add sprites and hole prefab

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerManager))]
+public class Managers : MonoBehaviour {
+
+    public static PlayerManager Player { get; private set; }
+
+    private void Awake()
+    {
+        Player = GetComponent<PlayerManager>(); 
+    }
+}
