9451695e27e7ad2d7b172d9f878d594b9b1edf39
cindyho97 <cindy.ho8197@gmail.com>
2017-09-25 16:03:06 +0200

    Add mole movement

--- Assets/Scripts/Mole.cs
+++ Assets/Scripts/Mole.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class Mole : MonoBehaviour {
 
-	// Use this for initialization
+    private Vector3 startingPosition;
+    private Vector3 endPosition;
+    private bool moveUp = false;
+    private bool moveDown = false;
+    // Time to take from start to finish
+    private float lerpTime = 0.5f;
+    private float currentLerpTime = 0f;
+
 	void Start () {
-		
+        startingPosition = transform.position;
+        endPosition = new Vector3(startingPosition.x, startingPosition.y + 100, startingPosition.z);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            moveUp = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            moveDown = true;
+        }
+ 
+        if (moveUp)
+        {
+            Popup();
+        }
+        else if (moveDown)
+        {
+            Hide();
+        }
+    }
+
+    private void Popup()
+    {
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        float speed = currentLerpTime / lerpTime;
+        transform.position = Vector3.Lerp(startingPosition, endPosition, speed);
+
+        if(transform.position == endPosition)
+        {
+            moveUp = false;
+            currentLerpTime = 0;
+        }
+    }
+
+    private void Hide()
+    {
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+            
+        }
+
+        float speed = currentLerpTime / lerpTime;
+        transform.position = Vector3.Lerp(endPosition, startingPosition, speed);
+
+        if(transform.position == startingPosition)
+        {
+            moveDown = false;
+            currentLerpTime = 0;
+        }
+    }
 }
